c06f5b273f4f26e55f7c6d71e9c00ef5a48ce9da
arthurnog <artuts874@gmail.com>
2020-10-21 11:06:38 -0300

    aprendendo estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App(){
+
+    const [projects, setProject] = useState(['seja feliz', 'não se odeie', 'não se cobre'])
+
+    //useState  retorna um array com duas posições
+    //1. variavel com valor inicial
+    //2. função pra atualizar o valor
+
+    function handleAddProject() {
+
+        setProject([...projects, `Amor ${Date.now()}`]);
+        //projects.push(`Amor ${Date.now()}`);
+
+        console.log(projects);
+    }
+
     return (
         <>
-            <Header title="tuts">
-                <ul>
-                    <li>seja feliz</li>
-                    <li>não se odeie</li>
-                    <li>não se cobre</li>
-                </ul>
-            </Header>
-            <Header title="projects" />
+            <Header title="tuts" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>venha para somar</button>
         </>
     );
 }
